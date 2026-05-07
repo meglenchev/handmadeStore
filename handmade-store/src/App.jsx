44b7dd66a2662cab9cb0e9499e0ca37ea6fb4220
meglenchev <t.meglenchev@gmail.com>
2026-05-07 09:07:32 +0300

    Implement automatic scroll to top on route change
    - Created ScrollToTopOnNavigation utility component using useLocation
    - Integrated the utility into App.jsx to ensure every page starts from the top
    - Improved user experience during navigation between different views

--- handmade-store/src/App.jsx
+++ handmade-store/src/App.jsx
@@ -4,10 +4,12 @@ import { Footer } from './components/layout/Footer.jsx';
 import { QuickView } from './components/common/QuickView.jsx';
 import { Home } from './pages/Home/Home.jsx';
 import { ScrollToTop } from './components/common/ScrollToTop.jsx';
+import { ScrollToTopOnNavigation } from './utils/ScrollToTopOnNavigation.jsx';
 
 function App() {
     return (
         <>
+            <ScrollToTopOnNavigation />
             <Header />
             <Home />
             <Footer />
---
+++ handmade-store/src/utils/ScrollToTopOnNavigation.jsx
@@ -0,0 +1,11 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router';
+
+export function ScrollToTopOnNavigation() {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
+    return null;
+}
