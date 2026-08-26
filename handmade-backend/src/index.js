15c3b51050488181ec7248230a089995be6fd33c
meglenchev <t.meglenchev@gmail.com>
2026-08-26 16:30:22 +0300

    feat: add verifyToken middleware and integrate cookie-parser
    - Install and configure cookie-parser
    - Implement verifyToken middleware for JWT validation via cookies
    - Add support for role-based authorization (RBAC)

--- handmade-backend/src/index.js
+++ handmade-backend/src/index.js
@@ -3,6 +3,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import { routes } from "./routes.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -27,6 +28,7 @@ app.use(
     }),
 );
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(routes);
---
+++ handmade-backend/src/middlewares/verifyToken.js
@@ -0,0 +1,39 @@
+import { jwt } from "jsonwebtoken";
+
+export function verifyToken(allowedRoles = []) {
+    return async (req, res, next) => {
+        const token = req.cookies.token;
+
+        if (!token) {
+            return res
+                .status(401)
+                .json({ message: "Access denied. No token provided." });
+        }
+
+        try {
+            const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+
+            req.user = decodedToken;
+
+            if (
+                allowedRoles.length > 0 &&
+                !allowedRoles.includes(req.user.role)
+            ) {
+                return res.status(403).json({
+                    message:
+                        "Forbidden: You do not have the required permissions.",
+                });
+            }
+
+            next();
+        } catch (err) {
+            if (err.name === "TokenExpiredError") {
+                return res
+                    .status(401)
+                    .json({ message: "Token expired. Please login again." });
+            }
+
+            res.status(401).json({ message: "Invalid token." });
+        }
+    };
+}
